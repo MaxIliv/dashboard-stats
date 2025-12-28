e2ddcdc93e0e3382b449fe65faa1ae01f3b44925
MaxIliv <max.r.iliv@gmail.com>
2025-12-28 20:11:24 +0200

    init

---
+++ api/users/stats.ts
@@ -0,0 +1,32 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+type Response = {
+  users: User[];
+  total: number;
+  skip: number;
+  limit: number;
+};
+type User = {
+  id: number;
+  age: number;
+};
+
+export default async function handler(
+  _req: VercelRequest,
+  res: VercelResponse
+) {
+  let response: Response = await fetch(
+    'https://dummyjson.com/users?limit=0'
+  ).then((res) => res.json());
+
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=300, stale-while-revalidate=600'
+  );
+
+  res.status(200).json({
+    total: 200,
+    medianAge: 32,
+    users: response?.total ?? 0,
+  });
+}
