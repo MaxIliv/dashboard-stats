a5b3cfdfad7676474a929b0700d2b668e88ba682
MaxIliv <max.r.iliv@gmail.com>
2026-01-02 19:45:10 +0200

    update chart data

--- api/users/chart.ts
+++ api/users/chart.ts
@@ -3,45 +3,51 @@ import { Response } from '../../types';
 
 type User = {
   birthDate: string;
-}
+};
 
 export const fetchUsersData = () =>
-  fetch(
-    'https://dummyjson.com/users?limit=0&select=birthDate'
-  ).then((res) => res.json());
-
+  fetch('https://dummyjson.com/users?limit=0&select=birthDate').then((res) =>
+    res.json()
+  );
 
 type Data = {
-  year: string;
-  count: number;
-}
+  year: number;
+  users: number;
+};
 
 const prepareChartData = (data: User[]): Data[] => {
-  const map = new Map<string, number>();
+  const map = new Map<number, number>();
 
   data.forEach((user) => {
-    const year = user.birthDate.substring(0, 4);
+    const year = Number(user.birthDate.substring(0, 4));
     map.set(year, (map.get(year) ?? 0) + 1);
-  })
+  });
 
   const res: Data[] = [];
 
-  map.forEach((count, year) => {
-    res.push({ year, count })
-  })
+  map.forEach((users, year) => {
+    res.push({ year, users });
+  });
+
+  res.sort((a, b) => a.year - b.year)
 
   return res;
-}
+};
 
 export default async function handler(
   _req: VercelRequest,
   res: VercelResponse
 ) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // or specific origin
 
-  let response: Response<User> = await fetchUsersData() as Response<User>;
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=300, stale-while-revalidate=600'
+  );
+
+  let response: Response<User> = (await fetchUsersData()) as Response<User>;
 
   const data = prepareChartData(response.users);
-
 
   res.status(200).json(data);
 }
